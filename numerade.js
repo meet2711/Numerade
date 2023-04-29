791c3b702a9e7faf6dda302fab8201c8dfde9773
meet2711 <meetshrimankar1@gmail.com>
2023-04-30 02:30:42 +0530

    try popup1

--- numerade.js
+++ numerade.js
@@ -211,15 +211,3 @@ function update() {
     col = 0; //start at 0 for new row
 }
 
-
-const popup = document.querySelector('.popup');
-const x = document.querySelector('.popup-content h1')
-
-window.addEventListener('load', () => {
-    popup.classList.add('showPopup');
-    popup.childNodes[1].classList.add('showPopup');
-})
-x.addEventListener('click', () => {
-    popup.classList.remove('showPopup');
-    popup.childNodes[1].classList.remove('showPopup');
-})

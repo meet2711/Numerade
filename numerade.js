be7ccfe7095c4a8c1d20327e8e5ccf77875a664d
meet2711 <meetshrimankar1@gmail.com>
2023-04-30 02:22:27 +0530

    test popup

--- numerade.js
+++ numerade.js
@@ -210,3 +210,31 @@ function update() {
     row += 1; //start new row
     col = 0; //start at 0 for new row
 }
+
+
+jQuery(document).ready(function ($) {
+
+    window.onload = function () {
+        $(".bts-popup").delay(1000).addClass('is-visible');
+    }
+
+    //open popup
+    $('.bts-popup-trigger').on('click', function (event) {
+        event.preventDefault();
+        $('.bts-popup').addClass('is-visible');
+    });
+
+    //close popup
+    $('.bts-popup').on('click', function (event) {
+        if ($(event.target).is('.bts-popup-close') || $(event.target).is('.bts-popup')) {
+            event.preventDefault();
+            $(this).removeClass('is-visible');
+        }
+    });
+    //close popup when clicking the esc keyboard button
+    $(document).keyup(function (event) {
+        if (event.which == '27') {
+            $('.bts-popup').removeClass('is-visible');
+        }
+    });
+});

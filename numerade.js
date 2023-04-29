872adc01e74e40f6c2477d532818ea4cc087c4bb
meet2711 <meetshrimankar1@gmail.com>
2023-04-30 02:27:46 +0530

    try popup

--- numerade.js
+++ numerade.js
@@ -212,29 +212,14 @@ function update() {
 }
 
 
-jQuery(document).ready(function ($) {
+const popup = document.querySelector('.popup');
+const x = document.querySelector('.popup-content h1')
 
-    window.onload = function () {
-        $(".bts-popup").delay(1000).addClass('is-visible');
-    }
-
-    //open popup
-    $('.bts-popup-trigger').on('click', function (event) {
-        event.preventDefault();
-        $('.bts-popup').addClass('is-visible');
-    });
-
-    //close popup
-    $('.bts-popup').on('click', function (event) {
-        if ($(event.target).is('.bts-popup-close') || $(event.target).is('.bts-popup')) {
-            event.preventDefault();
-            $(this).removeClass('is-visible');
-        }
-    });
-    //close popup when clicking the esc keyboard button
-    $(document).keyup(function (event) {
-        if (event.which == '27') {
-            $('.bts-popup').removeClass('is-visible');
-        }
-    });
-});
+window.addEventListener('load', () => {
+    popup.classList.add('showPopup');
+    popup.childNodes[1].classList.add('showPopup');
+})
+x.addEventListener('click', () => {
+    popup.classList.remove('showPopup');
+    popup.childNodes[1].classList.remove('showPopup');
+})
